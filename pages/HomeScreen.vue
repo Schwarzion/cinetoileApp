855424ef029bb80a6e6209642688eb2aa9defbe0
Thomas Vionnet <th.vionnet@gmail.com>
2021-02-02 17:58:33 +0100

    feat(update): add test API call and basic navigation

--- pages/HomeScreen.vue
+++ pages/HomeScreen.vue
@@ -1,5 +1,22 @@
 <template>
     <view>
         <text>Coucou</text>
+        <button title="Go to test" @press="goToTestScreen"></button>
     </view>
 </template>
+
+<script>
+export default {
+  // Declare `navigation` as a prop
+  props: {
+    navigation: {
+      type: Object
+    }
+  },
+  methods: {
+    goToTestScreen() {
+      this.navigation.navigate("Test");
+    }
+  }
+}
+</script>
--- pages/Test.vue
+++ pages/Test.vue
@@ -1,21 +1,42 @@
 <template>
   <view class="container">
-    <text>This is the App Details screen</text>
+    <text>Test {{ msg }}</text>
     <button title="Go to home screen" @press="goToHomeScreen"></button>
   </view>
 </template>
 
 <script>
+import { testApi } from "../services/test.service";
+
 export default {
+data() {
+    return {
+      msg: "",
+    };
+  },
+
   // Declare `navigation` as a prop
   props: {
     navigation: {
       type: Object
     }
   },
+
+  //Equivalent d'un ComponentDidMount ou d'un UseEffect en React -> gère le cycle de vie du composant
+  beforeMount() {
+    this.getMsg();
+  },
+
   methods: {
     goToHomeScreen() {
       this.navigation.navigate("Home");
+    },
+
+    async getMsg() {
+      const res = await testApi()
+
+      //this.msg correspond à la variable msg initialisé plus haut dans data()
+      this.msg = res.data;
     }
   }
 }
