5cecb024f05b69ff83e498f7f48d647477ee30e3
Lucas Poirier <lucas@tymate.com>
2021-04-30 03:15:13 +0200

    chore: platforms configuration

--- App.vue
+++ App.vue
@@ -1,29 +1,88 @@
 <template>
   <app-navigator></app-navigator>
 </template>
-
 <script>
 import {
   createAppContainer,
   createStackNavigator,
+  createBottomTabNavigator,
 } from "vue-native-router";
+import { THEME } from "./ui/index";
+import ROUTES from "./routes";
+import React from "react";
+import Icon from "react-native-vector-icons/FontAwesome5";
 
-import HomeScreen from "./pages/HomeScreen.vue";
-import Test from "./pages/Test.vue";
+/* Routes stacking bro, that's what i'm talking about, so much fun
+    Every bottom's bar icons will have their own navigation,
+    history and stuff. They're independant of each other.
+    To do so, you have to create for each icon a stackNavigator
+    that will contain every icon sub screens
+*/
 
-const StackNavigator = createStackNavigator(
+const HomeStack = createStackNavigator(
+  { stack: ROUTES.Home, Test: ROUTES.Like },
   {
-    Home: HomeScreen,
-    Test: Test,
-  },
-  {
-    initialRouteName: 'Home',
+    navigationOptions: {
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="home" solid size={22} color={tintColor} />
+      ),
+    },
   }
 );
 
-const AppNavigator = createAppContainer(StackNavigator);
+const LikeStack = createStackNavigator(
+  { stack: ROUTES.Like },
+  {
+    navigationOptions: {
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="heart" solid size={22} color={tintColor} />
+      ),
+    },
+  }
+);
+
+const ReservationStack = createStackNavigator(
+  { stack: ROUTES.Reservation },
+  {
+    navigationOptions: {
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="ticket-alt" size={22} color={tintColor} />
+      ),
+    },
+  }
+);
+
+const ProfileStack = createStackNavigator(
+  { stack: ROUTES.Profile },
+  {
+    navigationOptions: {
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="user-circle" solid size={22} color={tintColor} />
+      ),
+    },
+  }
+);
+
+const BottomTabNavigator = createBottomTabNavigator(
+  { HomeStack, LikeStack, ReservationStack, ProfileStack },
+  {
+    tabBarOptions: {
+      showLabel: false,
+      showIcon: true,
+      activeTintColor: THEME.primary,
+      inactiveTintColor: THEME.primaryAccent,
+    },
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "green",
+      },
+    },
+  }
+);
+
+const AppNavigator = createAppContainer(BottomTabNavigator);
 
 export default {
   components: { AppNavigator },
-}
+};
 </script>
